267b47945bf7a99fec78a04d1aa060389b43dee2
Durga-Prasad62 <majjidurgaprasad90@gmail.com>
2025-08-20 12:25:28 +0530

    updated

--- Armstrongwithinrange.py
+++ Armstrongwithinrange.py
@@ -24,3 +24,20 @@ def Armstrong_within_range(m,n):
      if updated==arm:
        print(arm,end=" ")
 Armstrong_within_range(1,1000)
+
+
+print("\n") # i used this for the new sepearation between two functions
+#for safer use temp=i
+def Armstrong_within_range(m,n):
+    for i in range(m,n+1):
+     updated=i
+     arm=0
+     length=len(str(i))
+     temp=i
+     while(temp!=0):
+        rem=temp%10
+        arm=arm+rem**length
+        temp//=10
+     if updated==arm:
+       print(arm,end=" ")
+Armstrong_within_range(1,1000)
