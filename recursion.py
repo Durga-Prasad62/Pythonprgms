5ed9ab38c70c6d0144cb3970717dea33c0d8d47b
Durga-Prasad62 <majjidurgaprasad90@gmail.com>
2025-09-26 21:04:13 +0530

    recurse

--- recursion.py
+++ recursion.py
@@ -2,22 +2,22 @@
 
 # Print first n natural numbers using recursion.
 
-# def natural(n):
-#     if n == 0:
-#         return 
-#     print(n)
-#     natural(n-1)
+def natural(n):
+    if n == 0:
+        return 
+    print(n)
+    natural(n-1)
 
-# natural(5)
+natural(5)
 
 # # N natural numbers using recursion in reverse order
-# def natural(n):
-#     if n == 0:
-#         return
-#     natural(n-1)
-#     print(n)
+def natural(n):
+    if n == 0:
+        return
+    natural(n-1)
+    print(n)
 
-# natural(10)
+natural(10)
 
 # First n even numbers using recursion
 
