c08346fed75c40795625abe63507a7f50d819cd0
Durga-Prasad62 <majjidurgaprasad90@gmail.com>
2025-09-26 21:01:58 +0530

    recurse

---
+++ recursion.py
@@ -0,0 +1,30 @@
+# Assignment 
+
+# Print first n natural numbers using recursion.
+
+# def natural(n):
+#     if n == 0:
+#         return 
+#     print(n)
+#     natural(n-1)
+
+# natural(5)
+
+# # N natural numbers using recursion in reverse order
+# def natural(n):
+#     if n == 0:
+#         return
+#     natural(n-1)
+#     print(n)
+
+# natural(10)
+
+# First n even numbers using recursion
+
+def even_number(n):
+    if n == 0:
+        return
+    even_number(n-1)
+    if n&1==0:
+     print(n)
+even_number(10)
