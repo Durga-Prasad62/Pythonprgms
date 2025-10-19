4aa793791b155f95eda27fe4af1ba4bc039ae7eb
Durga-Prasad62 <majjidurgaprasad90@gmail.com>
2025-10-20 00:26:46 +0530

    Update classtask.py

--- classtask.py
+++ classtask.py
@@ -1,36 +1,32 @@
-class Mobile():
-    def __init__(self,company,price,made_in,battery):
-        self.company=company
-        self.price=price
-        self.made_in=price
-        self.made_in=made_in
-        self.battery=battery
-        print("MOBILE COMPANY:" ,self.company)
-        print("MOBILE PRICE:" ,self.price)
-        print("MOBILE BATTERY:" ,self.battery)
-        print("MOBILE MADE IN:" ,self.made_in)
+class Mobile:
+    def __init__(self, company, price, made_in, battery):
+        self.company = company
+        self.price = price
+        self.made_in = made_in
+        self.battery = battery
+
     def display(self):
-        print("MOBILE COMPANY:" ,self.company)
-        print("MOBILE PRICE:" ,self.price)
-        print("MOBILE BATTERY:" ,self.battery)
-        print("MOBILE MADE IN:" ,self.made_in)
+        print("MOBILE COMPANY:", self.company)
+        print("MOBILE PRICE:", self.price)
+        print("MOBILE BATTERY:", self.battery)
+        print("MOBILE MADE IN:", self.made_in)
+
     @classmethod
-    def change_company_name(cls,new_name):
-        cls.newcompany=new_name
+    def change_company_name(cls, new_name):
+        cls.newcompany = new_name
+
     @staticmethod
     def new_update():
         print("Phone Update changed")
 
-mob=Mobile("apple",100000,"America","9000mah")
-mob.company="Vivo"
-print(mob.company)
-mob.price=10000
-print(mob.price)
-mob.made_in="India"
-print(mob.made_in)
-mob.battery="7000mah"
-print(mob.battery)
+# Example usage
+mob = Mobile("Apple", 100000, "America", "9000mah")
+mob.company = "Vivo"
+mob.price = 10000
+mob.made_in = "India"
+mob.battery = "7000mah"
 mob.display()
 mob.new_update()
 mob.change_company_name("Moto")
 print(mob.newcompany)
+
