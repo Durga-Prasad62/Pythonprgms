8c2df4d67c229960d00ee1bb8390e87fae7e1b9a
Durga-Prasad62 <majjidurgaprasad90@gmail.com>
2025-10-21 23:42:26 +0530

    Update sortingassign.py

--- sortingassign.py
+++ sortingassign.py
@@ -1,39 +1,46 @@
-# def BubbleSort (arr):
-#     for i in range(len(arr)):
-#         for j in range(len(arr)-i-1):
-#             if arr[j] < arr[j+1]:
-#                 arr[j],arr[j+1] = arr[j+1],arr[j]
-#     print(arr)
-       
+def bubble_sort_desc(arr):
+    for i in range(len(arr) - 1):
+        for j in range(len(arr) - 1 - i):
+            if arr[j] < arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
 
-# BubbleSort( [1,3,4,5,6,2,9,0,7])
-# BubbleSort( [67,0,67,-7,3,2,1,7,999])
-# BubbleSort( ["apple","bat","car","dog","elephant","fan"])
+print(bubble_sort_desc([1, 3, 4, 5, 6, 2, 9, 0, 7]))
+print(bubble_sort_desc([67, 0, 67, -7, 3, 2, 1, 7, 999]))
+
 
 #if we sort the strings if we give the answer based on the dictionary order
 
 
 # bubble  sort based on the length
-# def BubbleSort (arr):
-#     for i in range(len(arr)):
-#         for j in range(len(arr)-i-1):
-#            if len(arr[j])> len(arr[j+1]):
-#               arr[j],arr[j+1]=arr[j+1],arr[j]
-#     print(arr)      
-# BubbleSort( ["apple","bat","car","dog","elephant","fan"])
+def bubble_sort_strings(arr):
+    for i in range(len(arr) - 1):
+        for j in range(len(arr) - 1 - i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
 
-#bubble sort Can you sort nested lists based on the first element of each nested list.
+print(bubble_sort_strings(["apple", "bat", "car", "dog", "elephant", "fan"]))
 
-def BubbleSort(arr):
-    for i in range(len(arr)-1):
-        for j in range(len(arr)-1-i):
-            if arr[j][0] > arr[j+1][0]:
-                # Swap entire sublists
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-    print(arr)
+def bubble_sort_by_length(arr):
+    for i in range(len(arr) - 1):
+        for j in range(len(arr) - 1 - i):
+            if len(arr[j]) > len(arr[j + 1]):
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
 
-# Test
-BubbleSort([[1,56,56],[90,79,98],[78,86,72],[6,13,42],[89,7,5]])
+print(bubble_sort_by_length(["apple", "bat", "car", "dog", "elephant", "fan"]))
+
+bubble sort Can you sort nested lists based on the first element of each nested list.
+def bubble_sort_nested(arr):
+    for i in range(len(arr) - 1):
+        for j in range(len(arr) - 1 - i):
+            if arr[j][0] > arr[j + 1][0]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+print(bubble_sort_nested([[1, 56, 56], [90, 79, 98], [78, 86, 72], [6, 13, 42], [89, 7, 5]]))
+
 
 
 
